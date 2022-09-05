e3a9fdd1f2b27a2c9689a4584968aad2c69d797e
ccc <ccc>
2022-09-05 20:30:47 +0800

    TabBer

--- src/components/TabBer.vue
+++ src/components/TabBer.vue
@@ -1,29 +1,45 @@
 <template>
-    <div class="tabber">
-        
+  <div class="tabber">
+    <div class="tabber-img">
+      <img src="../assets/矩形 2.png" alt="" />
+      <img src="../assets/分组 3.png" alt="" />
+      <img src="../assets/分组 2.png" alt="" />
+      <img src="../assets/分组 1.png" alt="" />
     </div>
+  </div>
 </template>
   
   <script>
 export default {
-    name: 'TabBer',
-    props: {
-
-    }
-}
+  name: "TabBer",
+  props: {},
+};
 </script>
   
   <style scoped lang="scss">
-  .tabber {
-      position: fixed;
-      bottom: 24px;
-      width: 334px;
-      height: 66px;
-      border-radius: 50px;
-      background: rgba(255, 255, 255, 1);
-      border: 1px solid black;
-      margin-left: 21px ;
-      margin-right: 21px;
+.tabber {
+  position: fixed;
+  bottom: 24px;
+  width: 334px;
+  height: 66px;
+  border-radius: 50px;
+  background: rgba(255, 255, 255, 1);
+  margin-left: 21px;
+  margin-right: 21px;
+  .tabber-img {
+    width: 266.9px;
+    height: 18px;
+    display: flex;
+    flex-direction: row;
+    margin: 0 auto;
+    margin-top: 23px;
+    img {
+      width: 18px;
+      height: 18px;
+      object-fit:none ;
+      flex: 1;
+    }
   }
-  </style>
+}
+</style>
   
